ec6223a6f71c547202e117d82e453b125206e402
spahicadis <142044354+spahicadis@users.noreply.github.com>
2024-07-12 10:14:34 +0200

    First button

--- script.js
+++ script.js
@@ -2,17 +2,52 @@ const content = [
   {img: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M232.824 13.738c-96.1 7.382-215.29 48.535-215.29 118.383 0 65.084 117.615 107.844 245.49 93.538 90.757-10.15 167.13-45.817 200.578-89.152-44.08 82.325-143.703 106.34-233.877 116.19 112.254 5.15 262.53-32.988 262.53-124.23 0-64.527-117.615-107.84-245.49-93.534C156.007 45.082 80.41 80.75 46.96 124.084c18.41-49.265 91.46-93.87 185.864-110.346zm56.04 40.864c77.387-.618 136.667 23.666 136.667 58.234 0 4.73-1.112 9.433-3.227 14.053-31.82-20.172-88.055-36.5-149.174-39.148-6.105-.264-12.257-.387-18.437-.353-2.06.01-4.122.038-6.187.085-51.472 1.157-104.23 13.43-145.113 44.86 5.022-36.577 75.142-70.428 160.92-76.728 8.347-.613 16.547-.942 24.552-1.005zm-34.036 51.482c5.848-.037 11.685.077 17.492.328 57.486 2.49 111.856 19.132 139.096 35.758-6.62 6.792-15.386 13.238-25.896 19.14-24.89-11.45-63.462-19.06-105.92-19.142-3.244-.006-6.51.032-9.795.115-44.026 1.117-91.184 10.864-128.448 36.09-15.823-6.824-27.48-15.514-33.54-25.562 38.834-33.478 93.327-46.39 147.01-46.726zm24.717 54.79c31.283.173 60.834 5.14 82.178 11.675-27.063 10.793-60.804 18.506-97.41 21.194-37.686 2.768-72.323-.243-99.774-7.51 31.234-16.912 69.26-24.342 105.74-25.267 3.1-.08 6.19-.11 9.265-.092zM81.873 230.83c5.158 4.435 10.285 8.583 15.387 12.445.127.237.264.472.39.71l.592.023c61.57 46.215 119.69 52.326 188.065 51.99.002 0 .005 0 .007-.002v.002h-.007c-73.987 20.576-134.236 19.522-185.5-8.316 74.926 98.107 242.27 52.573 335.22-46.637-43.032 23.107-110.06 35.868-185.318 31.842-66.24-3.544-126.098-19.41-168.837-42.057zM324.55 335.342c-67.937 33.614-143.89 42.532-199.144 11.236 4.907 16.882 14.784 28.156 28.295 35.658 15.565 8.643 36.38 11.85 59 9.55 34.243-3.485 72.07-19.68 100.767-45.057 3.635-3.794 7.342-7.59 11.084-11.388zm-62.306 63.68c-15.773 5.91-31.93 9.757-47.652 11.357-23.025 2.342-45.308-.15-64.108-8.835 15.125 14.916 32.064 25.36 47.95 30.023 15.676 4.604 29.844 3.767 41.142-2.59 9.392-5.284 17.44-14.62 22.668-29.955zM139.22 416.985c7.508 25.64 18.886 51.543 38.15 77.282h38.546c.632-.79 1.39-1.33 1.75-2.364 5.113-14.602 11.056-28.235 17.715-41.15-13.33 3.696-27.91 2.948-42.212-1.252-18.734-5.5-37.433-16.752-53.95-32.516z"></path></svg>',
   heading: 'Get your home furnishings in a flash with rapid delivery.',
   text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, perspiciatis blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia.'
-   } 
+   },
+  {
+    img: '<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><desc></desc><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path><path d="M12 6l-3.293 3.293a1 1 0 0 0 0 1.414l.543 .543c.69 .69 1.81 .69 2.5 0l1 -1a3.182 3.182 0 0 1 4.5 0l2.25 2.25"></path><path d="M12.5 15.5l2 2"></path><path d="M15 13l2 2"></path></svg>',
+    heading: 'Always here to help: stellar support around the clock.',
+    text: 'Dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, plorem blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia minus sint, plorem ex harum.'
+  },
+  {
+    img: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>',
+    heading: 'Stay ahead of the trends with our latest discoveries.',
+    text: 'Ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, pLorem blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia temporibus.'
+  },
+  {
+    img: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M5.671 4.257c3.928-3.219 9.733-2.995 13.4.672 3.905 3.905 3.905 10.237 0 14.142-3.905 3.905-10.237 3.905-14.142 0A9.993 9.993 0 0 1 2.25 9.767l.077-.313 1.934.51a8 8 0 1 0 3.053-4.45l-.221.166 1.017 1.017-4.596 1.06 1.06-4.596 1.096 1.096zM13 6v2h2.5v2H10a.5.5 0 0 0-.09.992L10 11h4a2.5 2.5 0 1 1 0 5h-1v2h-2v-2H8.5v-2H14a.5.5 0 0 0 .09-.992L14 13h-4a2.5 2.5 0 1 1 0-5h1V6h2z"></path></g></svg>',
+    heading: 'No stress or worries for your home goods.',
+    text: 'Explicabo ipsum Error sit ametperspiciati consectetur voluptatibus elit. Error accusamus arc explicabo explictecto minus sint, se blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit sit amet architecto.'
+  }
+
 ]
+const img = document.querySelector('.img');
+const header = document.querySelector('.heder-rendered-js')
+const text = document.querySelector('.text-rendered-js')
+
+
 
 const buttons = document.querySelectorAll('.active')
 const buttonArr = [buttons[0],buttons[1],buttons[2]]
 
+function activeButtons() {
+  if(buttonArr[0]) {
+    img.innerHTML = content[0].img
+    header.innerHTML = content[0].heading
+    text.innerHTML = content[0].text
+  /*
+  } if(buttonArr[1]) {
+    img.innerHTML = content[1].img
+    header.innerHTML = content[1].heading
+    text.innerHTML = content[1].text
+    */
+  }
+}
+
+
 buttons.forEach(function(btn){
-btn.addEventListener('click', function(){
-  console.log('test');
+btn.addEventListener('click', activeButtons)
 });
-});
+
 
 
 
